806411f203608ca2f37642a281156b5fe8b583f5
Tatsuro Sakaguchi <tatsuro.sakaguchi@g.softbank.co.jp>
2021-01-04 18:11:24 +0900

    use cuda-drivers

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -23,12 +23,13 @@ sudo add-apt-repository \
      $(lsb_release -cs) \
      stable"
 
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
+curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+
 sudo apt update
 
-sudo apt install cmake code git google-chrome-stable docker-ce zsh make vim tmux solaar gnome-tweak-tool fcitx-mozc clang-format clang-tidy-10 guake global
-
-# install nvidia driver
-sudo ubuntu-drivers autoinstall
+sudo apt install cmake code git google-chrome-stable docker-ce nvidia-container-toolkit zsh make vim tmux solaar gnome-tweak-tool fcitx-mozc clang-format clang-tidy-10 guake global
 
 # install cuda 10.2
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-ubuntu1804.pin
@@ -37,7 +38,7 @@ wget http://developer.download.nvidia.com/compute/cuda/10.2/Prod/local_installer
 sudo dpkg -i cuda-repo-ubuntu1804-10-2-local-10.2.89-440.33.01_1.0-1_amd64.deb
 sudo apt-key add /var/cuda-repo-10-2-local-10.2.89-440.33.01/7fa2af80.pub
 sudo apt-get update
-sudo apt-get -y install cuda
+sudo apt-get -y install cuda cuda-drivers
 rm cuda-repo-ubuntu1804-10-2-local-10.2.89-440.33.01_1.0-1_amd64.deb
 
 # cudnn
