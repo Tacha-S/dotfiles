46bb0a70b03ecd407c50bb5c1f1140703436f3a1
Tatsuro Sakaguchi <tatsuro.sakaguchi@g.softbank.co.jp>
2021-01-22 12:04:01 +0900

    add script for generating compile commands

---
+++ generate_compile_commands_all.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -eu
+
+catkin build
+cat ./build/*/**/compile_commands.json > build/compile_commands.json && sed -i -e ':a;N;$!ba;s/\]\n*\[/,/g' build/compile_commands.json
+compdb -p build list > compile_commands.json
+
