24364c7f2220c4198568138ee4d81955c8e3d8c9
Tatsuro Sakaguchi <tatsuro.sakaguchi@g.softbank.co.jp>
2022-02-19 13:08:42 +0900

    Update script

--- update_extra_path.py
+++ update_extra_path.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
 
+import ast
 import json
 import pathlib
 
@@ -11,8 +12,17 @@ for setup_file in srcs.glob('**/setup.py'):
         continue
     if (setup_file.parent / 'CATKIN_IGNORE').exists():
         continue
-    my_extra_paths.append(str("${workspaceFolder}" / setup_file.parent / 'src'))
-# print(extra_path)
+    config = setup_file.parent / 'setup.cfg'
+    if not config.exists():
+        config.symlink_to(pathlib.Path.home() / 'work/.github/setup.cfg')
+    with open(setup_file, 'r') as f:
+        for line in f:
+            if 'package_dir' in line:
+                print(setup_file)
+                package_dir = ast.literal_eval(line.split('=')[1].strip(' \n\t,'))
+                for k, v in package_dir.items():
+                    if k == '':
+                        my_extra_paths.append(str("${workspaceFolder}" / setup_file.parent / v))
 
 with open('.vscode/settings.json', 'r') as f:
     settings = json.load(f)
