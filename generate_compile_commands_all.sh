8ea3a5567b0538db0f2e9672af780c417c45c64e
Tatsuro Sakaguchi <tatsuro.sakaguchi@g.softbank.co.jp>
2023-09-05 11:39:49 +0900

    Update script

--- generate_compile_commands_all.sh
+++ generate_compile_commands_all.sh
@@ -1,4 +1,4 @@
 #!/bin/zsh -eu
 
-cat ./build/*/**/compile_commands.json > build/compile_commands.json && sed -i -e ':a;N;$!ba;s/\]\n*\[/,/g' build/compile_commands.json
-compdb -p build list > compile_commands.json
+cat ../../build/*/**/compile_commands.json > ../../build/compile_commands.json && sed -i -e ':a;N;$!ba;s/\]\n*\[/,/g' ../../build/compile_commands.json
+compdb -p ../../build list > ../../compile_commands.json
