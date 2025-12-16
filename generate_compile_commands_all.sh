9887f5e5d3205a11e85e5d851461e476603a8efb
Tatsuro Sakaguchi <tatsuro.sakaguchi@g.softbank.co.jp>
2025-12-16 18:39:49 +0900

    Remove unnecessary script

--- generate_compile_commands_all.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/zsh -eu
-
-cat ../../build/*/**/compile_commands.json > ../../build/compile_commands.json && sed -i -e ':a;N;$!ba;s/\]\n*\[/,/g' ../../build/compile_commands.json
-compdb -p ../../build list > ../../compile_commands.json
